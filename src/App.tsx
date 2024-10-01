6515c1d51c95c1b5f4ea9bd0fc7b81ba4bf077be
tuananhhdev <tuananh@gmail.com>
2024-10-02 03:58:56 +0700

    first push

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import CustomersPage from "./pages/CustomersPage";
+
+function App() {
+  return (
+    <>
+      <CustomersPage />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/CustomersPage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const CustomersPage = () => {
+  const PlayfairDisplayFont = styled.h2`
+    font-family: "Playfair Display", serif;
+  `;
+
+  const RalewayFont = styled.p`
+    font-family: "Raleway", sans-serif;
+  `;
+  return <div>CustomersPage</div>;
+};
+
+export default CustomersPage;
